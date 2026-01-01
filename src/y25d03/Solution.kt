3b17b1f0d3c34ce4a2e1cdd812f9735a512f5e61
erikbabel <erik.babel@gmail.com>
2026-01-01 11:18:43 -0800

    2025 day 03 cleanup

--- src/y25d03/Solution.kt
+++ src/y25d03/Solution.kt
@@ -1,6 +1,5 @@
 package y25d03
 
-import isEven
 import println
 import readInput
 import solution
@@ -57,66 +56,21 @@ fun mapBank(bank: String, digits: Int): Long {
 
     var workingIndex = 0
 
-    var arr = bank.toCharArray()
-
-    var workingFinder = 1
+    val arr = bank.toCharArray()
 
     val withIndex = arr.withIndex()
 
 
 
     return (digits.downTo(1)).map { digit ->
-        val a = withIndex
+        val maxIndexedValue = withIndex
             .filter { it.index >= workingIndex && it.index < arr.size - (digit-1)}
             .maxBy { it.value.code }
-        println("max ${bank[a.index]} $a")
-        workingIndex = a.index + 1
-        a.value.toString()
+        println("max ${bank[maxIndexedValue.index]} $maxIndexedValue")
+        workingIndex = maxIndexedValue.index + 1
+        maxIndexedValue.value.toString()
     }.also {
         it.println()
     }.joinToString("").toLong()
 
-//    val b = arr.copyOfRange(a.index+1, arr.size).withIndex().maxBy { it.value.code }
-//    // first occurrence of the highest number
-//
-//    println("max ${bank[b.index]} $b")
-//    return a.value.toString().toInt() * 10 + b.value.toString().toInt()
-
-//    while (arr.size > digits && workingFinder < 9) {
-//        val indexOf = arr.indexOf(workingFinder.toString()[0])
-//        println("$workingFinder $indexOf ${arr.toList().toString()}")
-//        if (indexOf != -1) {
-//            arr = arr.copyOfRange(0,indexOf) + arr.copyOfRange(indexOf+1,arr.size)
-//
-//        } else {
-//            println("workingFinder $workingFinder")
-//            workingFinder++
-//        }
-//    }
-
-//    println(arr)
-//
-//    (1..digits).forEach {
-//        (9 downTo 1).forEach { a ->
-//            val indexOfA = bank.indexOf(a.toString(), workingIndex)
-//
-//        }
-//
-//    }
-//
-//
-//    (9 downTo 1).forEach { a ->
-//        val indexOfA = bank.indexOf(a.toString())
-//        if (indexOfA >= 0) {
-//            (9 downTo 1).forEach { b ->
-//                val indexOfB = bank.indexOf(b.toString(), indexOfA+1)
-//                if (indexOfB >= 0 && indexOfA < indexOfB) {
-//                    return a * 10 + b
-//                }
-//
-//            }
-//
-//        }
-//    }
-//    return arr.joinToString("").toInt()
 }
